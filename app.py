01ea5e0dc74a4d15292e35466b215818d7f5449b
XblazejX <jeniecblazej@gmail.com>
2025-08-04 18:40:51 +0200

    dodanie flaska

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    # Domyślnie wyświetl o-mnie.html
+    return send_from_directory('static', 'o-mnie.html')
+
+@app.route('/<path:path>')
+def serve_file(path):
+    # Obsłuż inne pliki statyczne (CSS, JS, itd.)
+    return send_from_directory('static', path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10196)
